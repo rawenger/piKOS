75f65cd2b2d27ffa14ef497c29d439913916c760
Ryan Wenger <ryanwenger@ucsb.edu>
2023-03-05 17:18:06 -0800

    adjust indentation

--- include/util/utils.h
+++ include/util/utils.h
@@ -4,7 +4,7 @@
 #define BIT(bit)			(1 << (bit))
 
 #define enable_irq()                    do { asm volatile ("\tmsr daifclr, #2\n"); } while (0)
-#define disable_irq()                    do { asm volatile ("\tmsr daifset, #2\n"); } while (0)
+#define disable_irq()                   do { asm volatile ("\tmsr daifset, #2\n"); } while (0)
 
 void delay (unsigned long);
 
@@ -14,4 +14,4 @@ extern void __memset_aarch64(void *p, int val, size_t n); //NOLINT(bugprone-rese
 extern void __memcpy_aarch64(void *restrict dest, const void *restrict src, size_t count); //NOLINT(bugprone-reserved-identifier)
 #define memcpy(dest, src, count)        __memcpy_aarch64(dest, src, count)
 extern void __memmove_aarch64(void *dest, const void *src, size_t count); //NOLINT(bugprone-reserved-identifier)
-#define memmove(dest, src, count)        __memmove_aarch64(dest, src, count)
+#define memmove(dest, src, count)       __memmove_aarch64(dest, src, count)
