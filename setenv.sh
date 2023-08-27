11c2c6f516e98390625085fc950031d256baa0b9
Ryan Wenger <ryanwenger@ucsb.edu>
2023-08-27 16:16:48 -0700

    Update toolchain and JTAG configuration

--- setenv.sh
+++ setenv.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 #
-# Environment setup for pi-kos development
-export PATH="$HOME/pi-kos/arm-gnu-toolchain-12.2/bin:$PATH"
+# Linux environment setup for pi-kos development
+TOOLCHAIN_HOME='/opt/arm-gnu-toolchain'
+
+# only add to PATH if we aren't already there
+[[ ":$PATH:" == *":$TOOLCHAIN_HOME/bin:"* ]] || export PATH="$TOOLCHAIN_HOME/bin:$PATH"
+
+# ARM's GDB executable breaks without these settings. Conveniently, the system/host GDB breaks *with* them.
+export PYTHONPATH='/usr/lib64/python38.zip:/usr/lib64/python3.8:/usr/lib64/python3.8/lib-dynload:/usr/lib64/python3.8/site-packages:/usr/lib/python3.8/site-packages'
+export PYTHONHOME='/usr'
